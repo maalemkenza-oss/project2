4735b2dd7ed8b0c5b92072461d3044c765805e32
maalemkenza-oss <maalem.kenza@univ-oeb.dz>
2025-12-26 15:06:27 +0100

    Update seqalgo.erl

--- seqalgo.erl
+++ seqalgo.erl
@@ -4,18 +4,33 @@
 -import(sets,[from_list/1,to_list/1,subtract/2,add_element/2]).
 -import(codinglist,[coding_list/1]).
 -import(library,[split/2,is_Terminal/2,getInitialConf/1,displayOfConf/2,countSetBits/1,second/2]).
-generate(Refvec,S,T)->   
-             case S of 
-                [Conf|Tl]-> B=not(is_Terminal(Conf,Refvec)),B2= (countSetBits(second(Conf,Refvec)) >= math:sqrt(length(Refvec))),
-
-
+ 
+generate(Refvec, S) ->
+    case S of 
+        [Conf|Tl]-> 
+            B = not(is_Terminal(Conf, Refvec)),
+            B2 = (countSetBits(second(Conf, Refvec)) >= math:sqrt(length(Refvec))),
+            
             if 
-               (B and B2) -> {Rs1,Rs2}=split(Conf,Refvec),
-                                                 L=[Rs1,Rs2],S1=append(L,Tl),generate(Refvec,S1,T);
-               (not(B)and B2) -> T1=append([Conf],T),generate(Refvec,Tl,T1);
-                 not(B2) -> generate(Refvec,Tl,T)
-
-   
+                (B and B2) -> 
+                    {Rs1, Rs2} = split(Conf, Refvec),
+                    % Exploration en profondeur d'abord: essayer Rs1
+                    case generate(Refvec, [Rs1|Tl]) of
+                        none -> 
+                            % Si Rs1 ne donne pas de solution, essayer Rs2
+                            generate_one_helper(Refvec, [Rs2|Tl]);
+                        Solution -> 
+                            Solution  % Solution trouvée, la retourner
+                    end;
+                    
+                (not(B) and B2) -> 
+                    % Configuration terminale valide = SOLUTION TROUVÉE
+                    Conf;  % Retourner immédiatement
+                    
+                not(B2) -> 
+                    % Configuration non valide, continuer
+                    generate_one_helper(Refvec, Tl)
             end;
-             []-> T
-  end.
+        []-> 
+            none  % Pas de solution dans cette branche
+    end.
